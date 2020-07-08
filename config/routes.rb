e7fe86b843227b5206ce811e629ac87f525a94a6
Your Name <ishiguro-tomoya@MacBook-Pro.lan>
2020-07-08 18:38:46 +0900

    [Add]投稿に対するいいね機能

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   root 'posts#index'
   resources :posts, only: [:new, :create, :index, :show] do
+   resource :favorites, only: [:create, :destroy]
    resources :post_comments, only: [:create, :destroy]
  end
 end
