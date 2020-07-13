e942afcf9a8e5a8faf095ac56c988a431e28c297
Your Name <ishiguro-tomoya@MacBook-Pro.lan>
2020-07-13 10:39:05 +0900

    [Modified]投稿詳細ページのレイアウト

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
+# post=投稿　投稿に対して　いいね　コメント機能が存在する親子関係（ネスト）
   root 'posts#top'
   resources :posts, only: [:top, :new, :create, :index, :show, :destroy] do
    resource :favorites, only: [:create, :destroy]
