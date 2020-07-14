e6cd3acbfcf7161abb95cad58dca23f354f7e480
Your Name <ishiguro-tomoya@MacBook-Pro.lan>
2020-07-14 17:41:03 +0900

    [Add]投稿の編集機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  
+
   # devise_scope :user do
   #   delete '/users/sign_out' => 'devise/sessions#destroy'
   # end
 
 # post=投稿　投稿に対して　いいね　コメント機能が存在する親子関係（ネスト）
   root 'posts#top'
-  resources :posts, only: [:top, :new, :create, :index, :show, :destroy] do
+  resources :posts, only: [:top, :new, :create, :index, :show, :destroy, :edit, :update] do
    resource :favorites, only: [:create, :destroy]
    resources :post_comments, only: [:create, :destroy]
  end
